f6724f06ff3c2aa7d6584d78f5a0107da23fdbc6
prince chouhan <pc75805@gmail.com>
2022-05-11 14:56:23 +0530

    obj update

--- src/controllers/Bookcontroller.js
+++ src/controllers/Bookcontroller.js
@@ -2,7 +2,7 @@ const bookModel = require("../models/BookModel")
 const UserModel = require("../models/UserModel")
 const validator = require("../validator/validator")
 const mongoose = require("mongoose")
-const isValidObjectId = (objectId) => { return mongoose.Types.ObjectId.isValid(objectId) }
+const isValidObj2ectId = (obj2ectId) => { return mongoose.Types.Obj2ectId.isValid(obj2ectId) }
 const moment=require('moment')
 
 
@@ -10,7 +10,7 @@ const createBook = async function (req, res) {
     try {
         const data = req.body
         let { title, excerpt, userId, ISBN, category, subcategory } = data
-        if (Object.keys(data).length == 0) {
+        if (Obj2ect.keys(data).length == 0) {
             return res.status(400).send({ status: false, message: "EMPTY INPUT" })
         }
         
@@ -41,7 +41,7 @@ const createBook = async function (req, res) {
         if (!validator.isValid(excerpt)) {
             return res.status(400).send({ status: false, message: "EXCERPT IS NOT VALID" })
         }
-        if (!isValidObjectId(userId)) {
+        if (!isValidObj2ectId(userId)) {
             return res.status(400).send({ status: false, message: "NOT A VALID USER ID" });
         }
        let decodedToken= req.decodedToken
@@ -57,7 +57,7 @@ const createBook = async function (req, res) {
         if (!validator.isValid(category)) {
             return res.status(400).send({ status: false, message: "CATEGORY IS NOT VALID" })
         }
-        if (typeof (subcategory) == "object") {
+        if (typeof (subcategory) == "obj2ect") {
             for (let i = 0; i < subcategory.length; i++) {
                 if (!validator.isValid(subcategory[i]))
                     return res.status(400).send({ status: false, message: "SUB-CATEGORY IS NOT VALID" })
@@ -97,7 +97,7 @@ const getBooks = async function (req, res) {
     try {
         const { userId, category, subcategory } = req.query
 
-        if (Object.keys(req.query).length == 0) {
+        if (Obj2ect.keys(req.query).length == 0) {
             let find = await bookModel.find({ isDeleted: false })
             console.log(find)
             res.status(200).send({ status: true, message:"BooksList",data:find })
@@ -106,34 +106,34 @@ const getBooks = async function (req, res) {
         // CHECK USER ID VALIDATION------
         else if (userId || category || subcategory) {
             if (userId) {
-                let isValid = mongoose.Types.ObjectId.isValid(userId);
+                let isValid = mongoose.Types.Obj2ectId.isValid(userId);
                 if (!isValid) { return res.status(400).send({ status: false, message: " USER-Id is Not Valid" }) }
             }
-            // TAKE OBJECT OBJ WITH CONDITION ISDELETE = FALSE------
+            // TAKE OBJ2ECT OBJ2 WITH CONDITION ISDELETE = FALSE------
             const obj = {
                 isDeleted: false
             }
             if (userId)
                 obj.userId = userId.trim();
-            // TAKE ANOTHER OBJECT -----  
-            //const obj = {}
+            // TAKE ANOTHER OBJ2ECT -----  
+            const obj2 = {}
             if (category) {
-                obj.category = category
+                obj2.category = category
             }
             if (subcategory)
-                obj.subcategory = subcategory
+                obj2.subcategory = subcategory
             // MAKE A FOR LOOP FOR SPLITING AND TRIMING GIVEN FILTERS------
-            for (let key in obj) {
-                if (typeof (obj[key]) == "string") {
-                    obj[key] = obj[key].split(",")
+            for (let key in obj2) {
+                if (typeof (obj2[key]) == "string") {
+                    obj2[key] = obj2[key].split(",")
                    
                 }
-                for (let i = 0; i < obj[key].length; i++)
-                    obj[key][i] = obj[key][i].toLowerCase().trim()
-                obj[key] = { $all: obj[key] }
+                for (let i = 0; i < obj2[key].length; i++)
+                    obj2[key][i] = obj2[key][i].toLowerCase().trim()
+                obj2[key] = { $all: obj2[key] }
             }
             //FIND BOOK WITH THE HELP OF GIVEN FILTERS------
-            const data = await bookModel.find({ ...obj }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
+            const data = await bookModel.find({ ...obj2 , ...obj }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
 
             // IF  NO BOOK FOUND WITH GIVEN FILTERS-----
             if (data.length == 0) {
@@ -159,7 +159,7 @@ const getBooksById = async function(req,res){
     }
 
     // BOOK ID VALIDATION-----
-    let isValid = mongoose.Types.ObjectId.isValid(bookId);
+    let isValid = mongoose.Types.Obj2ectId.isValid(bookId);
     if (!isValid) { return res.status(400).send({ status: false, message: " BOOK-Id is Not Valid"})}
 
     // FIND BOOKS BY BOOK ID-----
@@ -183,10 +183,10 @@ const updateBook = async function (req, res) {
     try {
         let getId = req.params.bookId
         let data = req.body
-        if (!isValidObjectId(getId)) {
+        if (!isValidObj2ectId(getId)) {
             return res.status(400).send({ status: false, message: "NOT A VALID ID" });
         }
-        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "KINDLY ADD SOMETHING TO UPDATE" })
+        if (Obj2ect.keys(data).length == 0) return res.status(400).send({ status: false, message: "KINDLY ADD SOMETHING TO UPDATE" })
         let { title, excerpt, ISBN } = data
 
         let checkId = await bookModel.findById(getId)
