6683cfecf45fc3041d8cb090701d5574a13bfca7
KANISHKMAKKAR <kanishk.makkar.333@gmail.com>
2022-05-12 11:20:53 +0530

    DATE VALIDATION PROPERLY DONE

--- src/controllers/ReviewController.js
+++ src/controllers/ReviewController.js
@@ -1,15 +1,27 @@
 const { isValidObjectId, default: mongoose, set } = require("mongoose")
 const BookModel = require("../models/BookModel")
 const ReviewModel = require("../models/ReviewModel")
+const UserModel = require("../models/UserModel")
 
 
 const createReview = async (req,res)=>{
     try{
         let {reviewedBy, rating, review} = req.body
         let bookId = req.params.bookId
+        if(!isValidObjectId(bookId)){
+            return res.status(400).send({status:false, message:"'BOOK ID' is Invalid!"})
+        }
+
+        const isBookExist = await BookModel.findOne({_id:bookId})
+        if(!isBookExist){
+            return res.status(404).send({status:false, message:"no book found with given ID"})
+        }
+        if(isBookExist.isDeleted==true){
+            return res.status(404).send({status:false, message:"DELETED BOOKS CANT BE REVIEWED"})
+        }
 
         if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, message: "No data in request body" })
-
+         
         if(!reviewedBy){
             return res.status(400).send({status:false, message:"'reviewedBy' is empty!"})
         }
@@ -19,17 +31,15 @@ const createReview = async (req,res)=>{
         if(rating < 1 || rating > 5){
             return res.status(401).send({status:false, message:"Invalid value of rating, It should be betweeen 1 and 5"})
         }
-        if(!review){
-            return res.status(400).send({status:false, message:"'review' is empty/not given"})
+        let checkuser =await UserModel.findOne({name:reviewedBy})
+        if(!checkuser){
+            return res.status(404).send({status:false, message:"NO USER FOUND"})
         }
-        if(!isValidObjectId(bookId)){
-            return res.status(400).send({status:false, message:"'BOOK ID' is Invalid!"})
+        if(checkuser.isDeleted==true){
+            return res.status(400).send({status:false, message:" USER DELETED"})
         }
-
-        const isBookExist = await BookModel.findOne({bookId:bookId, isDeleted:false })
-        if(!isBookExist){
-            return res.status(404).send({status:false, message:"no book found with given ID/ is Already Deleted"})
-        }
+        
+       
 
         let newReview = {
             bookId:bookId,
@@ -40,10 +50,6 @@ const createReview = async (req,res)=>{
         }
 
         let createdReview = await ReviewModel.create(newReview) 
-
-        if(!createdReview){
-            return res.status(400).send({status:false, message:"no Review Doc is created!"})
-        }
 
         let Reviews = await ReviewModel.find({bookId:bookId, isDeleted:false})
         let count = Reviews.length
--- src/validator/validator.js
+++ src/validator/validator.js
@@ -10,8 +10,9 @@ const isTitleValid= function(value){
     return false
 }
 const isPhoneValid = function (value){
-    if (!value) return false
-    if(value.length!=10) return false
+    if(! /^[6-9]\d{9}$/.test(value)){
+        return false
+    }
     return true
 }
 const isEmailValid = function(value){
