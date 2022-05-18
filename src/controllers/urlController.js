3fb2c7d44772e4a09aa6017391f621f3d4875b55
prince chouhan <pc75805@gmail.com>
2022-05-18 15:47:31 +0530

    add cache

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,9 +1,31 @@
 const urlModel = require("../models/urlModel")
 const validUrl = require('valid-url')
 const shortid = require('short-id')
+const redis = require("redis");
+
+const { promisify } = require("util");
 
 // THIS IS BASE URL-----
 const baseUrl = 'http:localhost:3000'
+
+//Connect to redis
+const redisClient = redis.createClient(
+    13190,
+    "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+  });
+
+  //Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const shortUrl = async function (req, res) {
     try {
@@ -22,18 +44,49 @@ const shortUrl = async function (req, res) {
 
         // GENERATE URLCODE------
         const urlCode = shortid.generate()
+        // const urlCode = "428d08"
         console.log(urlCode)
-
-        // FIND LONG URL ALRADY PRESENT OR NOT IN DB----
-        let url = await urlModel.findOne({ longUrl })
-
-        if (url) {
-            return res.status(400).send({ status: false, message: "URL alrady shorted" })
+        
+        // FIND DOCUMENT CONTAINING SAME URLCODE PRESENT OR NOT IN DB OR IN CASH IF IT IS IN DB SET INTO CACHE-----
+        let cahcedUrlCodeData = await GET_ASYNC(`${urlCode}`)
+        if(cahcedUrlCodeData) {
+                return res.status(400).send({ status: false, message: "UrlCode alrady present in CACHE" })
+        } else {
+            const findUrlCode = await urlModel.findOne({urlCode:urlCode})
+          await SET_ASYNC(`${urlCode}`, JSON.stringify(findUrlCode))
+          if(findUrlCode){
+            return res.status(400).send({ status: false, message: "UrlCode alrady present in db" })
+        }
         }
 
+        // FIND LONG URL ALRADY PRESENT OR NOT IN DB OR IN CASH IF IT IS IN DB SET INTO CACHE----
+        let cahcedLongUrlData = await GET_ASYNC(`${longUrl}`)
+        if(cahcedLongUrlData) {
+                return res.status(400).send({ status: false, message: "Url alrady shorted in CACHE" })
+        } else {
+            let url = await urlModel.findOne({ longUrl })
+          await SET_ASYNC(`${longUrl}`, JSON.stringify(url))
+          if (url) {
+            return res.status(400).send({ status: false, message: "URL alrady shorted in db" })
+        }
+        }
+        
         // CREATE SHORT URL----
         const shortUrl = baseUrl + '/' + urlCode
+        // const shortUrl ="http:localhost:3000/bd1ba0"
         console.log(shortUrl)
+
+        // FIND DOCUMENT HAVING SAME SHORTURL PRESENT OR NOT IN DB OR IN CASH IF IT IS IN DB SET INTO CACHE-----
+        let cahcedShortUrlData = await GET_ASYNC(`${shortUrl}`)
+        if(cahcedShortUrlData) {
+                return res.status(400).send({ status: false, message: "Short-Url alrady present in CACHE" })
+        } else {
+            const findShortUrl = await urlModel.findOne({shortUrl:shortUrl})
+          await SET_ASYNC(`${shortUrl}`, JSON.stringify(findShortUrl))
+          if(findShortUrl){
+            return res.status(400).send({ status: false, message: "Short-Url alrady present in db" })
+        }
+        }
 
         // MAKE OBJECT CONTAINING ALL MANDATORY FIELDS-----
         let createUrl = {
@@ -53,6 +106,7 @@ const shortUrl = async function (req, res) {
 // REDIRECTING TO THE LONG URL----
 const redirectUrl = async function (req, res) {
     try {
+        //FETCH URLCODE FROM PARAMS----
         let shortId = req.params.urlCode
 
         // FINDING IN DATABASE----
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const urlController = require('../controllers/urlController')
 
 router.post('/url/shorten',urlController.shortUrl)
+
 router.get('/:urlCode',urlController.redirectUrl)
 
 module.exports=router
